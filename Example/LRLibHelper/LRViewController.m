99abe60018b8625fe43976f1496da4a10173f98f
李睿 <dgg0906@sina.com>
2021-12-17 10:18:54 +0800

    修改

--- Example/LRLibHelper/LRViewController.m
+++ Example/LRLibHelper/LRViewController.m
@@ -7,8 +7,6 @@
 //
 
 #import "LRViewController.h"
-#import "LRLibHeader.h"
-#import "TestViewController.h"
 
 @interface LRViewController ()
 
@@ -21,18 +19,13 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    UIButton * btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [btn addTarget:self action:@selector(btnas) forControlEvents:UIControlEventTouchUpInside];
-    [btn setTitle:@"aaaaa" forState:UIControlStateNormal];
-    [btn setBackgroundColor: [UIColor lightGrayColor]];
-    [self.view addSubview:btn];
+    
 
 }
 
 - (void)btnas
 {
-    TestViewController * vc = [TestViewController new];
-    [self presentViewController:vc animated:YES completion:nil];
+    
 }
 
 @end
